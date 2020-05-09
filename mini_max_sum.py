b8f1867617631c4cf3ac66501721aa4312582467
Apurvaa <sapurvaa@yahoo.in>
2020-05-09 19:46:00 -0400

    Mini Max Sum

---
+++ mini_max_sum.py
@@ -0,0 +1,22 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the miniMaxSum function below.
+def miniMaxSum(arr):
+    arr.sort()
+    min_sum,max_sum = 0,0
+    for i in range(len(arr)-1):
+        min_sum += arr[i]
+    for i in range(1,len(arr)):
+        max_sum += arr[i]
+    print(min_sum,max_sum)
+
+if __name__ == '__main__':
+    arr = list(map(int, input().rstrip().split()))
+
+    miniMaxSum(arr)
