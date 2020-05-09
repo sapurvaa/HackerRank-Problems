4b89eb91bbd90ff5e846dddb31bd4b1e9b722ec4
Apurvaa <sapurvaa@yahoo.in>
2020-05-09 19:50:08 -0400

    Counting Valleys

---
+++ counting_valleys.py
@@ -0,0 +1,34 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the countingValleys function below.
+def countingValleys(n, s):
+    valleys = 0
+    pair = 0
+    for i in s:
+        if i == "U":
+            pair += 1
+        else:
+            pair -= 1
+        if pair == 0 and i == "U":
+            valleys += 1
+
+    return valleys
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    s = input()
+
+    result = countingValleys(n, s)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
